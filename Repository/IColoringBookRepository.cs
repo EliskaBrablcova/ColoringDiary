713045068803ced6297d3dfed9dd8affc56d3d5a
Eliška Brablcová <eliska.brablcova@gmail.com>
2020-05-17 23:17:28 +0200

    Coloring book repository creation

---
+++ Repository/IColoringBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eli.ColoringDiary.Repository
+{
+	public interface IColoringBookRepository
+	{
+		// Add new, Edit, Delete, Detail 
+	}
+}
